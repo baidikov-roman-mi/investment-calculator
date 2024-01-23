8bdb2fbb3cd466273fb383d85870da4f09dd1424
Baidikov Roman <103574775+baidikov-roman-mi@users.noreply.github.com>
2024-01-23 19:52:32 +0200

    Done with expected UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="user-input">
+        <div className="input-group">
+          <div>
+            {" "}
+            {/* expect Input component */}
+            <label>Initial investment</label>
+            <input type="number" />
+          </div>
+          <div>
+            {/* expect Input component */}
+            <label>Annual investment</label>
+            <input type="number" />
+          </div>
+          <div>
+            {/* expect Input component */}
+            <label>Expected Return</label>
+            <input type="number" />
+          </div>
+          <div>
+            {/* expect Input component */}
+            <label>Duration</label>
+            <input type="number" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div id="result" className="center">
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">Year</th>
+              <th scope="col">Investment Value</th>
+              <th scope="col">Interest(Year)</th>
+              <th scope="col">Total Interest</th>
+              <th scope="col">Invested Capital</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>1</td>
+              <td>10</td>
+              <td>100</td>
+              <td>1000</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>1</td>
+              <td>10</td>
+              <td>100</td>
+              <td>1000</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>1</td>
+              <td>10</td>
+              <td>100</td>
+              <td>1000</td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>1</td>
+              <td>10</td>
+              <td>100</td>
+              <td>1000</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
